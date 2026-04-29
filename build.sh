e9dc342c63b33da08af22fdf1fd7b8e0f3875ea3
Mathieu ABATI <mathieu.abati@gmail.com>
2026-04-29 14:02:48 +0200

    Add fast build script

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 set -e
 
-# Build Contribs
-mkdir -p contrib/native
-pushd contrib/native
-../bootstrap
-make -j${BB_BUILD_JOBS:-2}
-popd
+FAST=${1}
+
+if [ ! -d contrib/native ] || [[ "${FAST}" != "1" ]]; then
+	echo "Standard build mode: building contribs..."
+	# Build Contribs
+	mkdir -p contrib/native
+	pushd contrib/native
+	../../contrib/bootstrap
+	make -j${BB_BUILD_JOBS:-2}
+	popd
+else
+	echo "Fast build mode: skipping contrib build"
+fi
 
 # Build VLC
-# We use the prefix to install into the BuildBox target directory
-./bootstrap
-./configure --prefix="${BB_TARGET_BUILD_DIR}" --disable-chromecast --disable-asdcp
+if [ ! -f config.h ] || [[ "${FAST}" != "1" ]]; then
+	echo "Standard build mode: bootstrapping and configuring..."
+	./bootstrap
+	./configure --prefix="${BB_TARGET_BUILD_DIR}" --disable-chromecast --disable-asdcp
+else
+	echo "Fast build mode: skipping bootstrap and configuration"
+fi
+
+echo "Building VLC..."
 make -j${BB_BUILD_JOBS:-2}
 make install
